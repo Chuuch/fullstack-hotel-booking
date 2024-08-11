e1168962e5d8c81f5f16b5186426598e7b411ba0
Chuuch <daniel.chuchulev96@gmail.com>
2024-08-11 04:43:09 -0700

    Custom User Details Service

---
+++ src/main/java/com/chuchulev/hotel_booking/service/CustomUserDetailsService.java
@@ -0,0 +1,20 @@
+package com.chuchulev.hotel_booking.service;
+
+import com.chuchulev.hotel_booking.exception.OurException;
+import com.chuchulev.hotel_booking.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Username not found!"));
+    }
+}
