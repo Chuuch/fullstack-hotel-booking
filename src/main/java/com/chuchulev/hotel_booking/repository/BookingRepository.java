baae23841fb6935ef808387815e48913fe90fb49
Chuuch <daniel.chuchulev96@gmail.com>
2024-08-11 04:29:06 -0700

    Repositories

---
+++ src/main/java/com/chuchulev/hotel_booking/repository/BookingRepository.java
@@ -0,0 +1,11 @@
+package com.chuchulev.hotel_booking.repository;
+
+import com.chuchulev.hotel_booking.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+
+    Optional<Booking> findByBookingConfirmationCode(String confirmationCode);
+}
